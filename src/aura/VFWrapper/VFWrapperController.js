5fd21364a17396e17ef1e418f66a474ee0294c3c
marco2207 <drake2207@gmail.com>
2020-02-12 15:04:42 +0100

    Update VFWrapperController.js

--- src/aura/VFWrapper/VFWrapperController.js
+++ src/aura/VFWrapper/VFWrapperController.js
@@ -4,10 +4,12 @@
 		window.addEventListener("message", function(event) {
 			if (event.origin !== vfOrigin) {
 				// Not the expected origin: reject message
+				console.log("@@@ Wrong Origin !!");
 				return;
 			}
 			// Only handle messages we are interested in
 			if (event.data.name === "com.mycompany.chatmessage") {
+				console.log("@@@ Right message arrived!!);
 				var vfMessages = component.get("v.vfMessages");
 				vfMessages = vfMessages + event.data.payload + "\n";
 				component.set("v.vfMessages", vfMessages);
@@ -18,6 +20,7 @@
 	sendToVF : function(component, event, helper) {
         var vfOrigin = "https://" + component.get("v.vfHost");
 		var vfWindow = component.find("vfFrame").getElement().contentWindow;
+		console.log("@@@ Sending message to VF !!");
 		var message = {
 			name: "com.mycompany.chatmessage",
 			payload: component.get("v.message")
